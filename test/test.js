779fabcf09461d417e3f79e82d4de0f51a1c495a
Bruno Dufour <dufour@iro.umontreal.ca>
2013-03-16 22:32:17 -0400

    - Added more unit tests

--- test/test.js
+++ test/test.js
@@ -76,7 +76,7 @@ describe('Instrumentation', function() {
 
     describe('Calls', function() {
         it('should handle calls to function declarations', function() {
-            var script = "function f() {}; f();";
+            var script = "var z = 0; function f() {z=1;}; f();";
             var state = instrumentAndExecScript(script);
             assertCounters({
                 onMethodEntry: 1,
@@ -84,10 +84,11 @@ describe('Instrumentation', function() {
                 onBeforeCall: 1,
                 onAfterCall: 1
             }, state["esprof$counters"]);
+            assert.equal(1, state.z);
         });
 
         it('should handle calls to function expressions', function() {
-            var script = "var f = function () {}; f();";
+            var script = "var z = 0; var f = function () {z=1;}; f();";
             var state = instrumentAndExecScript(script);
             assertCounters({
                 onMethodEntry: 1,
@@ -95,10 +96,11 @@ describe('Instrumentation', function() {
                 onBeforeCall: 1,
                 onAfterCall: 1
             }, state["esprof$counters"]);
+            assert.equal(1, state.z);
         });
 
         it('should handle calls via simple properties', function() {
-            var script = "var x = {f: function () {}}; x.f();";
+            var script = "var z = 0; var x = {f: function () {z=1;}}; x.f();";
             var state = instrumentAndExecScript(script);
             assertCounters({
                 onMethodEntry: 1,
@@ -106,11 +108,11 @@ describe('Instrumentation', function() {
                 onBeforeCall: 1,
                 onAfterCall: 1
             }, state["esprof$counters"]);
-
+            assert.equal(1, state.z);
         });
 
         it('should handle calls via deeply nested properties', function() {
-            var script = "var x = {y: {z: {f: function () {}}}}; x.y.z.f();";
+            var script = "var z = 0; var x = {y: {z: {f: function () {z=1;}}}}; x.y.z.f();";
             var state = instrumentAndExecScript(script);
             assertCounters({
                 onMethodEntry: 1,
@@ -118,14 +120,171 @@ describe('Instrumentation', function() {
                 onBeforeCall: 1,
                 onAfterCall: 1
             }, state["esprof$counters"]);
+            assert.equal(1, state.z);
         });
     });
 
-    describe('Property reads', function() {
-        // TODO
-    });
+    describe('Properties', function() {
+        it('should handle property reads with dot notation', function() {
+            var script = "var x = {p: 42}; var y = x.p;";
+            var state = instrumentAndExecScript(script);
+            assert.equal(1, state["esprof$counters"].onPropRead);
+            assert.equal(42, state.y);
+        });
 
-    describe('Property writes', function() {
-        // TODO
+        it('should handle property reads with [] notation & string literal name', function() {
+            var script = "var x = {p: 42}; var y = x['p'];";
+            var state = instrumentAndExecScript(script);
+            assert.equal(1, state["esprof$counters"].onPropRead);
+            assert.equal(42, state.y);
+        });
+
+        it('should handle property reads with [] notation & expression as name', function() {
+            var script = "var p = 'p'; var x = {p: 42}; var y = x[p];";
+            var state = instrumentAndExecScript(script);
+            assert.equal(1, state["esprof$counters"].onPropRead);
+            assert.equal(42, state.y);
+        });
+
+        it('should handle property writes with dot notation', function() {
+            var script = "var x = {p: 42}; x.p = 'abc'; var y = x.p;";
+            var state = instrumentAndExecScript(script);
+            assert.equal(1, state["esprof$counters"].onPropWrite);
+            assert.equal('abc', state.y);
+        });
+
+        it('should handle property writes with [] notation & string literal name', function() {
+            var script = "var x = {p: 42}; x['p'] = 'abc'; var y = x.p;";
+            var state = instrumentAndExecScript(script);
+            assert.equal(1, state["esprof$counters"].onPropWrite);
+            assert.equal('abc', state.y);
+        });
+
+        it('should handle property writes with [] notation & expression as name', function() {
+            var script = "var p = 'p'; var x = {p: 42}; x[p] = 'abc'; var y = x.p;";
+            var state = instrumentAndExecScript(script);
+            assert.equal(1, state["esprof$counters"].onPropWrite);
+            assert.equal('abc', state.y);
+        });
+
+        it('should handle += property update', function() {
+            var script = "var x = {n: 5}; x.n += 4;";
+            var state = instrumentAndExecScript(script);
+            assertCounters({
+                onPropRead: 1,
+                onPropWrite: 1
+            }, state["esprof$counters"]);
+            assert.equal(9, state.x.n);
+        });
+
+        it('should handle *= property update', function() {
+            var script = "var x = {n: 5}; x.n *= 4;";
+            var state = instrumentAndExecScript(script);
+            assertCounters({
+                onPropRead: 1,
+                onPropWrite: 1
+            }, state["esprof$counters"]);
+            assert.equal(20, state.x.n);
+        });
+
+        it('should handle /= property update', function() {
+            var script = "var x = {n: 5}; x.n /= 4;";
+            var state = instrumentAndExecScript(script);
+            assertCounters({
+                onPropRead: 1,
+                onPropWrite: 1
+            }, state["esprof$counters"]);
+            assert.equal(1.25, state.x.n);
+        });
+
+        it('should handle -= property update', function() {
+            var script = "var x = {n: 5}; x.n -= 4;";
+            var state = instrumentAndExecScript(script);
+            assertCounters({
+                onPropRead: 1,
+                onPropWrite: 1
+            }, state["esprof$counters"]);
+            assert.equal(1, state.x.n);
+        });
+
+        it('should handle %= property update', function() {
+            var script = "var x = {n: 23}; x.n %= 4;";
+            var state = instrumentAndExecScript(script);
+            assertCounters({
+                onPropRead: 1,
+                onPropWrite: 1
+            }, state["esprof$counters"]);
+            assert.equal(3, state.x.n);
+        });
+
+        it('should handle <<= property update', function() {
+            var script = "var x = {n: 23}; x.n <<= 4;";
+            var state = instrumentAndExecScript(script);
+            assertCounters({
+                onPropRead: 1,
+                onPropWrite: 1
+            }, state["esprof$counters"]);
+            assert.equal(368, state.x.n);
+        });
+
+        it('should handle >>= property update', function() {
+            var script = "var x = {n: 368}; x.n >>= 4;";
+            var state = instrumentAndExecScript(script);
+            assertCounters({
+                onPropRead: 1,
+                onPropWrite: 1
+            }, state["esprof$counters"]);
+            assert.equal(23, state.x.n);
+        });
+
+        it('should handle >>>= property update', function() {
+            var script = "var x = {n: -255}; x.n >>>= 20;";
+            var state = instrumentAndExecScript(script);
+            assertCounters({
+                onPropRead: 1,
+                onPropWrite: 1
+            }, state["esprof$counters"]);
+            assert.equal(4095, state.x.n);
+        });
+
+        it('should handle &= property update', function() {
+            var script = "var x = {n: 23}; x.n &= 10;";
+            var state = instrumentAndExecScript(script);
+            assertCounters({
+                onPropRead: 1,
+                onPropWrite: 1
+            }, state["esprof$counters"]);
+            assert.equal(2, state.x.n);
+        });
+
+        it('should handle |= property update', function() {
+            var script = "var x = {n: 23}; x.n |= 10;";
+            var state = instrumentAndExecScript(script);
+            assertCounters({
+                onPropRead: 1,
+                onPropWrite: 1
+            }, state["esprof$counters"]);
+            assert.equal(31, state.x.n);
+        });
+
+        it('should handle ^= property update', function() {
+            var script = "var x = {n: 23}; x.n ^= 10;";
+            var state = instrumentAndExecScript(script);
+            assertCounters({
+                onPropRead: 1,
+                onPropWrite: 1
+            }, state["esprof$counters"]);
+            assert.equal(29, state.x.n);
+        });
+
+        it('should handle chained updates', function() {
+            var script = "var x = {n: 5}; x.n *= x.n *= x.n *= x.n;";
+            var state = instrumentAndExecScript(script);
+            assertCounters({
+                onPropRead: 4,
+                onPropWrite: 3
+            }, state["esprof$counters"]);
+            assert.equal(625, state.x.n);
+        });
     });
 });
